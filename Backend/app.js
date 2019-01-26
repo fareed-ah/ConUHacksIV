3ee1f44f61d58f513c4c28fa587b706e8f96437f
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2019-01-26 14:46:59 -0500

    Added ReactStrap

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+   res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Started");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 
 
 //import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
