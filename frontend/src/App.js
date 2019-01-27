b8ae1b8bc9bb3f81d858643fb9f3b82b39ed42f8
Hari Govindasamy <Hari.Govindasamy@pwbcorp.com>
2019-01-27 02:14:28 -0500

    it went to shit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,104 @@
-import React, { Component } from "react";
-// import logo from './logo.svg';
-// import './App.css';
+// // import React, { Component } from "react";
+
+// // // import logo from './logo.svg';
+// // // import './App.css';
 
 
-//import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
+// // //import React, { Component } from "react";
+// // import 'bootstrap/dist/css/bootstrap.min.css';
+// // import axios from "axios";
 
-class App extends Component {
+// // class App extends Component {
 
-  state = {
-    data: [],
-    id: 0,
-    address: "Carleton",
-    price: "100.00",
-    intervalIsSet: false,
-  };
-
-
-  // when component mounts, first thing it does is fetch all existing data in our db
-    // then we incorporate a polling logic so that we can easily see if our db has
-    // changed and implement those changes into our UI
-    componentDidMount() {
-      this.getDataFromDb();
-      if (!this.state.intervalIsSet) {
-        let interval = setInterval(this.getDataFromDb, 100000);
-        this.setState({ intervalIsSet: interval });
-      }
-    }
-
-    // never let a process live forever
-    // always kill a process everytime we are done using it
-    componentWillUnmount() {
-      if (this.state.intervalIsSet) {
-        clearInterval(this.state.intervalIsSet);
-        this.setState({ intervalIsSet: null });
-      }
-    }
-
-    // our first get method that uses our backend api to
-  // fetch data from our data base
-  getDataFromDb = () => {
-    fetch("http://localhost:3001/getData")
-      .then(data => data.json())
-      .then(res => this.setState({ data: res.data }));
-  };
-
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = () => {
-    // let currentIds = this.state.data.map(data => data.id);
-    let idToBeAdded = 0;
-
-    axios.post("http://localhost:3001/getData", {
-      id: idToBeAdded,
-      address: "carleton university", 
-      price: "133000"
-    });
-  };
+// //   state = {
+// //     data: [],
+// //     id: 0,
+// //     address: "Carleton",
+// //     price: "100.00",
+// //     intervalIsSet: false,
+// //   };
 
 
-  render() {
-  	const data = this.state.data; 
-  	return <div> 
-	IM READY TO USE THE BACK END APIS! 
-	{JSON.stringify(data)}; 
+// //   // when component mounts, first thing it does is fetch all existing data in our db
+// //     // then we incorporate a polling logic so that we can easily see if our db has
+// //     // changed and implement those changes into our UI
+// //     componentDidMount() {
+// //       this.getDataFromDb();
+// //       if (!this.state.intervalIsSet) {
+// //         let interval = setInterval(this.getDataFromDb, 100000);
+// //         this.setState({ intervalIsSet: interval });
+// //       }
+// //     }
+
+// //     // never let a process live forever
+// //     // always kill a process everytime we are done using it
+// //     componentWillUnmount() {
+// //       if (this.state.intervalIsSet) {
+// //         clearInterval(this.state.intervalIsSet);
+// //         this.setState({ intervalIsSet: null });
+// //       }
+// //     }
+
+// //     // our first get method that uses our backend api to
+// //   // fetch data from our data base
+// //   getDataFromDb = () => {
+// //     fetch("http://localhost:3001/getData")
+// //       .then(data => data.json())
+// //       .then(res => this.setState({ data: res.data }));
+// //   };
+
+// //   // our put method that uses our backend api
+// //   // to create new query into our data base
+// //   putDataToDB = () => {
+// //     // let currentIds = this.state.data.map(data => data.id);
+// //     let idToBeAdded = 0;
+
+// //     axios.post("http://localhost:3001/getData", {
+// //       id: idToBeAdded,
+// //       address: "carleton university", 
+// //       price: "133000"
+// //     });
+// //   };
+
+
+// //   render() {
+// //   	const data = this.state.data; 
+// //   	return <div> 
+// // 	IM READY TO USE THE BACK END APIS! 
+// // 	{JSON.stringify(data)}; 
 
 
 
-  	</div>
+// //   	</div>
 
-  }
-}
+// //   }
+// // }
 
-	export default App;
+// // 	export default App;
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// };
+
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={14}
+//         style={mapStyles}
+//         initialCenter={{
+//          lat: -1.2884,
+//          lng: 36.8233
+//         }}
+//       />
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: 'YOUR_GOOGLE_API_KEY_GOES_HERE'
+// })(MapContainer);
---
+++ frontend/src/MapComponent.js
@@ -0,0 +1,137 @@
+// import React, { Component } from "react";
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// };
+
+
+
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={14}
+//         style={mapStyles}
+//         initialCenter={{
+//          lat: -1.2884,
+//          lng: 36.8233
+//         }}
+//       />
+//     );
+//   }
+// }
+
+
+// export default GoogleApiWrapper extends component({
+//   apiKey: 'AIzaSyD1F_n5E1N1b_xtjczOoGgDWe88Id6csr0'
+// })(MapContainer);
+
+// // // const MapStyle = {
+// // // 	height: 80%
+
+// // // };
+
+
+// // function InitMap() {
+// //         // Styles a map in night mode.
+// //         return new google.maps.Map(document.getElementById('map'), { center: {lat: -34.397, lng: 150.644},
+// //   zoom: 8}); 
+// //           //center: {lat: 40.674, lng: -73.945},
+// //           //: 12
+// //           // styles: [
+// //           //   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+// //           //   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+// //           //   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+// //           //   {
+// //           //     featureType: 'administrative.locality',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#d59563'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'poi',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#d59563'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'poi.park',
+// //           //     elementType: 'geometry',
+// //           //     stylers: [{color: '#263c3f'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'poi.park',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#6b9a76'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road',
+// //           //     elementType: 'geometry',
+// //           //     stylers: [{color: '#38414e'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road',
+// //           //     elementType: 'geometry.stroke',
+// //           //     stylers: [{color: '#212a37'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#9ca5b3'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road.highway',
+// //           //     elementType: 'geometry',
+// //           //     stylers: [{color: '#746855'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road.highway',
+// //           //     elementType: 'geometry.stroke',
+// //           //     stylers: [{color: '#1f2835'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'road.highway',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#f3d19c'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'transit',
+// //           //     elementType: 'geometry',
+// //           //     stylers: [{color: '#2f3948'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'transit.station',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#d59563'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'water',
+// //           //     elementType: 'geometry',
+// //           //     stylers: [{color: '#17263c'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'water',
+// //           //     elementType: 'labels.text.fill',
+// //           //     stylers: [{color: '#515c6d'}]
+// //           //   },
+// //           //   {
+// //           //     featureType: 'water',
+// //           //     elementType: 'labels.text.stroke',
+// //           //     stylers: [{color: '#17263c'}]
+// //           //   }
+// //           // ]
+        
+// //               }
+
+// // class MapComponent extends Component{
+// // 	render(){
+// // 		return(
+// //     <div id="map" >
+// //    	{this.InitMap()}
+// //    	</div>
+// // 	);}
+
+// // }
+// // export default MapComponent;
